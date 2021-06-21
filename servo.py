fa2a99723e026563949d2c636488a5a6373ee6d6
unknown <500075823@stu.upes.ac.in>
2021-06-21 21:40:44 +0530

    Added Servo Test Code

--- servo.py
+++ servo.py
@@ -5,3 +5,23 @@ Created on Mon Jun 21 18:36:33 2021
 @author: Neel
 """
 
+
+import RPi.GPIO as GPIO
+from time import sleep
+
+def servoAngle(angle):
+	duty = angle / 18 + 2
+	GPIO.output(11, True)
+	p.ChangeDutyCycle(duty)
+	sleep(1)
+	GPIO.output(11, False)
+	p.ChangeDutyCycle(0)
+    
+
+
+GPIO.setup(11, GPIO.OUT)
+p = GPIO.PWM(11, 50)
+p.start(0)
+servoAngle(90)
+sleep(2)
+servoAngle(0)
