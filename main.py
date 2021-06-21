44daae5eafed09014af80b63018d7d9f4ab037ef
unknown <500075823@stu.upes.ac.in>
2021-06-21 18:45:10 +0530

    Added Object Detection Function

--- main.py
+++ main.py
@@ -13,6 +13,18 @@ import io
 
 os.environ["GOOGLE_APPLICATION_CREDENTIALS"]="/home/pi/Downloads/cognitio-7378d-f0c72d841d75.json"
 
+def detect(img):
+    from google.cloud import vision
+    client = vision.ImageAnnotatorClient()
+    file_name = os.path.abspath(img)
+    with io.open(file_name, 'rb') as image_file:
+        content = image_file.read()
+    
+    image = vision.Image(content=content)
+    response = client.label_detection(image=image)
+    labels = response.label_annotations
+    return labels
+
 camera = PiCamera()
 camera.rotation = 180
 
