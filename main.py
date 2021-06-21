ad73a6103e94ba4ec29132769345ebf65085275f
unknown <500075823@stu.upes.ac.in>
2021-06-21 22:34:24 +0530

    Merged main with servo

--- main.py
+++ main.py
@@ -11,6 +11,8 @@ from time import sleep
 import os
 import io
 
+
+
 os.environ["GOOGLE_APPLICATION_CREDENTIALS"]="/home/pi/Downloads/cognitio-7378d-f0c72d841d75.json"
 
 def detect(img):
@@ -25,6 +27,24 @@ def detect(img):
     labels = response.label_annotations
     return labels
 
+
+
+def servoAngle(angle):
+	duty = angle / 18 + 2
+	GPIO.output(2, True)
+	p.ChangeDutyCycle(duty)
+	sleep(1)
+	GPIO.output(2, False)
+	p.ChangeDutyCycle(0)
+    
+    
+
+GPIO.setmode(GPIO.BCM)
+
+GPIO.setup(2, GPIO.OUT)
+p = GPIO.PWM(2, 50)
+p.start(0)
+
 camera = PiCamera()
 camera.rotation = 180
 
@@ -35,4 +55,4 @@ camera.capture('a.jpg')
 s = detect("a.jpg")
 for i in s:
     print(i.description)
-print(s)
+    
